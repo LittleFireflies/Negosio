f8f704969f799432d8d400c87589c9e779f89cd0
Fadhil Ikhsan <f.ikhsanta@gmail.com>
2017-01-18 14:15:52 +0700

    Add recycle view

--- app/src/main/java/id/sch/smktelkom_mlg/projectwork/negosio/board/HomeBoard.java
+++ app/src/main/java/id/sch/smktelkom_mlg/projectwork/negosio/board/HomeBoard.java
@@ -1,24 +1,26 @@
 package id.sch.smktelkom_mlg.projectwork.negosio.board;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
 import id.sch.smktelkom_mlg.projectwork.negosio.R;
+import id.sch.smktelkom_mlg.projectwork.negosio.adapter.HotelAdapter;
 import id.sch.smktelkom_mlg.projectwork.negosio.database.UserLogin;
 import id.sch.smktelkom_mlg.projectwork.negosio.helper.LoginHelper;
+import id.sch.smktelkom_mlg.projectwork.negosio.model.Hotel;
 import io.realm.Realm;
 
 public class HomeBoard extends Fragment implements View.OnClickListener{
@@ -27,6 +29,8 @@ public class HomeBoard extends Fragment implements View.OnClickListener{
     Context ctx;
     Realm realm;
     LoginHelper loginHelper;
+    ArrayList<Hotel> mList = new ArrayList<>();
+    HotelAdapter mAdapter;
     private String username = "";
     private TextView tvUsername;
     private int size = 0;
@@ -46,7 +50,6 @@ public class HomeBoard extends Fragment implements View.OnClickListener{
         assignToView();
         getUserData();
         onSetView();
-
         return rootView;
     }
 
@@ -63,11 +66,37 @@ public class HomeBoard extends Fragment implements View.OnClickListener{
     }
 
     private void assignToView() {
-        tvUsername = (TextView) rootView.findViewById(R.id.tvUser);
+        //tvUsername = (TextView) rootView.findViewById(R.id.tvUser);
+        //Script recycle view;
+
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
+        GridLayoutManager layoutManager = new GridLayoutManager(ctx, 2);
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new HotelAdapter(mList);
+        recyclerView.setAdapter(mAdapter);
+
+        fillData();
+        // Akhir
     }
 
     @Override
     public void onClick(View view) {
 
+    }
+
+    private void fillData() {
+        Resources resources = getResources();
+        String[] arJudul = resources.getStringArray(R.array.places);
+        String[] arDeskripsi = resources.getStringArray(R.array.place_desc);
+        TypedArray a = resources.obtainTypedArray(R.array.places_picture);
+        Drawable[] arFoto = new Drawable[a.length()];
+        for (int i = 0; i < arFoto.length; i++) {
+            arFoto[i] = a.getDrawable(i);
+        }
+        a.recycle();
+        for (int i = 0; i < arJudul.length; i++) {
+            mList.add(new Hotel(arJudul[i], arDeskripsi[i], arFoto[i]));
+        }
+        mAdapter.notifyDataSetChanged();
     }
 }
