e952cb06a9236fc95dab320eab535404aa1c6ed5
LittleFireflies <widyjp10@gmail.com>
2017-04-11 07:11:43 +0700

    Insyaallah fix (final)

--- app/src/main/java/id/sch/smktelkom_mlg/projectwork/negosio/IntroActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/projectwork/negosio/IntroActivity.java
@@ -43,6 +43,7 @@ public class IntroActivity extends AppIntro {
     }
 
     private void loadMainActivity() {
+        finish();
         startActivity(new Intent(this, MainActivity.class));
     }
 }
